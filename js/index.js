45cdadb7242e02d061d09335f09ea62dabf2ae85
Juan Gallardo <juangallardovergara17@gmail.com>
2021-04-22 17:20:15 -0400

    Manipular el dom y agregar el tinymce

--- js/index.js
+++ js/index.js
@@ -1,5 +1,28 @@
-document.querySelector("#registrar-btn").addEventListener("click", ()=>{
-    let nombre =  document.querySelector("#nombre-txt").value;
 
-    alert("Hola Mundo!" + nombre);
+tinymce.init({
+    selector: '#desc-txt',
+    height: 200,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+    'bold italic backcolor | alignleft aligncenter ' +
+    'alignright alignjustify | bullist numlist outdent indent | ' +
+    'removeformat | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+  
+document.querySelector("#registrar-btn").addEventListener("click", ()=>{
+    //value es para obtener el valor de los input de txt
+    let nombre =  document.querySelector("#nombre-txt").value;
+    //para obtener lo escrito en la desc, sacado de tinymce
+    let descripcion = tinymce.get("desc-txt").getContent();
+    //checked indica si el radiobutton esta seleccionado
+    let legendario =  document.querySelector("#legend-si").checked;
+    //El tipo se obtiene igual que los input
+    let tipo = document.querySelector("#tipo-select").value;
+    console.log("Hola Mundo!" + nombre,descripcion,legendario,tipo);
 } );
